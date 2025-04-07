e340b5f33c5649b872bae8299c76391ee90c10f2
shestakovitch <shestakovitch@gmail.com>
2025-04-07 23:52:56 +0200

    Added config.py

---
+++ config.py
@@ -0,0 +1,3 @@
+base_url = "https://prenotami.esteri.it"
+salter_1_url = "https://prenotami.esteri.it/Services/Booking/1151"
+salter_2_url = "https://prenotami.esteri.it/Services/Booking/1258"
--- main.py
+++ main.py
@@ -1,9 +1,33 @@
-from selenium_login import login
+import os
+from dotenv import load_dotenv
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from config import base_url, salter_1_url, salter_2_url
+
+# Читаем переменные окружения
+load_dotenv()
+LOGIN = os.getenv("LOGIN")
+PASSWORD = os.getenv("PASSWORD")
 
 
 def main():
-    login()
+    """Функция логинится по ссылке base_url"""
+    driver = webdriver.Chrome()
+    driver.get(url=base_url)
 
+    # Вводим логин
+    email_element = driver.find_element(By.ID, value="login-email")
+    email_element.clear()
+    email_element.send_keys(LOGIN)
+
+    # Вводим пароль
+    password_element = driver.find_element(By.ID, value="login-password")
+    password_element.clear()
+    password_element.send_keys(PASSWORD)
+    password_element.send_keys(Keys.RETURN)
+
+    driver.get(url=salter_1_url)
 
 if __name__ == "__main__":
     main()
